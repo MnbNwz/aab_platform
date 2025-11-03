85608f1ae2ff3782d4b50e5863a8bfdc24bf0fb2
muneeb <muneebnawaz2018@gmail.com>
2025-11-03 21:52:35 +0500

    table filter fixed

--- client/src/components/dashboard/JobManagementTable.tsx
+++ client/src/components/dashboard/JobManagementTable.tsx
@@ -322,7 +322,7 @@ const JobManagementTable: React.FC = memo(() => {
                 Filters:
               </span>
             </div>
-            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4">
+            <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
               {/* Status Filter */}
               <div>
                 <label
